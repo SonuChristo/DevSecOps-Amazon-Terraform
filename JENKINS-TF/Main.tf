6a09b01689ad4fcd188016bc36baf34048df29bd
SonuChristo <sonuchristo455@gmail.com>
2023-10-29 19:21:20 +0530

    new commit

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "web" {
   user_data              = templatefile("./install_jenkins.sh", {})
 
   tags = {
-    Name = "Jenkins-sonar"
+    Name = "amazon-Ec2"
   }
   root_block_device {
     volume_size = 30
